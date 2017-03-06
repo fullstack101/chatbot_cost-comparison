c09a397da0920fe2e30b2613372a2369712fc691
nasko <naskodi@live.com>
2017-03-06 22:06:28 +0200

    functions

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.get('/', function(req,res) {
     res.send("Hi, I'm a chatbot");
 });
 
+let token = "EAAa4Wnh0ewUBAFupVsHWsX9tdkUVMBHVZAtJQEF1cLF4qZCguDShji7WvEh7XQZCO0L7itkGjOxpsiWFaD23k8NRDwHEHFjKYvHqJDi0zZAKjV98H3F6lz0MvbuovJn6d92FlfepoYksrRiwikQLRZCKZBiitOqHjtn7FUcZBF2xwZDZD";
+
 //FACEBOOK
 
 /**app.get('/webhook/', function(res,req) {
@@ -40,6 +42,39 @@ app.get('/webhook', function(req, res) {
         res.sendStatus(403);
     }
 });
+
+app.post('/webhook', function(res,req){
+   let messaging_events= req.body.entry[0].messaging;
+   for (let i=0; i<messaging_events.length; i++){
+       let event=messaging_events[i];
+       let sender = event.sender.id;
+       if (event.message && event.message.text){
+           let text = event.message.text;
+           sendText(sender, "Text echo: " + text.substring(0,100));
+       }
+   }
+   res.sendStatus(200);
+});
+
+function sendText(sender, text) {
+    let messageData = {text: text};
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: token},
+        method: "POST",
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }), function(error, response, body){
+        if (error){
+            console.log("sending error");
+        }
+        else if(response.body.error){
+            console.log("response body error");
+        }
+    }
+}
 
 app.listen(app.get('port'),function(res,req) {
     console.log("running: port");
