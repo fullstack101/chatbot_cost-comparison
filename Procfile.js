f152868179b6b25277e83984067bc98b4d480aa5
nasko <naskodi@live.com>
2017-03-06 20:57:50 +0200

    initial

---
+++ Procfile.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Atanas on 06-Mar-17.
+ */
+web: node index.js
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Atanas on 06-Mar-17.
+ */
+'use strict';
+
+const express = requre('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//Allow us to process data
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//ROUTES
+
+app.get('/', function(req,res) {
+    res.send("Hi, I'm a chatbot");
+});
+
+//FACEBOOK
+
+app.get('/webhook/', function(res,req) {
+    if(req.query['hub.verify_token'] === "token987") {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token");
+});
+
+app.listen(app.get('port'),function(res,req) {
+    console.log("running: port");
+});
