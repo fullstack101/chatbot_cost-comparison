355e173ee835932c1198576aaa5ba5109a0e4c87
nasko <naskodi@live.com>
2017-03-12 20:15:56 +0200

    bot

--- index.js
+++ index.js
@@ -43,13 +43,14 @@ app.get('/webhook', function(req, res) {
     }
 });
 
-let frame = "";
+//let frame = "";
 
 app.post('/webhook/', function(req, res) {
     let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
         let event = messaging_events[i];
         let sender = event.sender.id;
+        let frame="";
         //sendText(sender, "Frame: "+frame);
         if (event.message && event.message.text) {
             let text = event.message.text;
