0956110654e7a174ccb20065b795377098ad3f2a
nasko <naskodi@live.com>
2017-03-12 20:25:31 +0200

    bot

--- index.js
+++ index.js
@@ -49,12 +49,13 @@ app.post('/webhook/', function(req, res) {
     let messaging_events = req.body.entry[0].messaging;
     let frame="";
     for (let i = 0; i < messaging_events.length; i++) {
+
         let event = messaging_events[i];
         let sender = event.sender.id;
         //sendText(sender, "Frame: "+frame);
         if (event.message && event.message.text) {
             let text = event.message.text;
-
+            sendText(sender, "Frame: "+frame);
             //sendText(sender, "Text echo: " + text.substring(0, 100))
             let greeting = "Hi, do you want to see a cost comparisson between your city and Blagoevgrad, Bulgaria?";
             /*if(text=="hi")
